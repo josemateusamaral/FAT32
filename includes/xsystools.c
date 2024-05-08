d09a6ba9a0fb8a18263890556836dbf54ab16521
José Mateus Amaral de Miranda <50233573+josemateusamaral@users.noreply.github.com>
2024-05-07 22:12:16 -0300

    Add files via upload

---
+++ includes/xsystools.c
@@ -0,0 +1,190 @@
+/*
+    Esta função recebe o caminho de um arquivo e grava ele em uma imagem de disco.
+    Ela realiza primeiro a inserção do arquivo na pasta root, depois coloca os bytes do arquivo
+    no cluster e depois configura as informações do cluster.
+    Parametros:
+        nomeDoArquivo: contem uma string que indica o arquivo a ser gravado no disco
+        nomeDisco: contem uma string indicando o nome do disco para o qual a imagem sera gravada
+*/
+
+void gravarArquivo( char* nomeDoArquivo, char* nomeGravacao ) {
+
+    int bytes_lidos;
+    unsigned char *bufferLeituraEscrita = (unsigned char *)malloc( 512 * sizeof(unsigned char) );
+
+
+    //testes de criacao de arquivo usando a chamada de sistema
+    FILE * arquivoEntrada = fopen(nomeDoArquivo,"rb");
+    XFILE fileNoFat = xopen(nomeGravacao);
+    while(true){
+        bytes_lidos = fread(&bufferLeituraEscrita[0], 1, 200, arquivoEntrada);
+        if(!bytes_lidos){
+            break;
+        }else{
+            xwrite(bufferLeituraEscrita,1,bytes_lidos,&fileNoFat);
+        }
+    }
+    fclose(arquivoEntrada);
+    xclose(&fileNoFat);
+
+}
+
+
+/*
+    Esta função serve para pegar um arquivo de dentro do disco FAT32 e retorna-lo como um arquivo fora do disco FAT32
+    Parametros:
+        nomeArquivo: Uma string com o nome do arquivo que esta dentro do disco FAT32
+        disco: Uma string com o nome de um disco FAT32
+        nomeFinalArquivo: uma string com o nome que será usado para gravar o arquivo fora do disco FAT32.
+*/
+void copiarArquivo(char* nomeArquivo, char* nomeFinalArquivo){
+
+    //buffer usado para leitura e escrita e numero de bytes da leitura
+    int bytes_lidos;
+    unsigned char *bufferLeituraEscrita = (unsigned char *)malloc( 512 * sizeof(unsigned char) );
+
+    //testar a copia de um arquivo do disco FAT32 para fora
+    FILE * arquivoSaida = fopen(nomeFinalArquivo,"wb");
+    XFILE fileDentro = xopen(nomeArquivo);
+    while(true){
+        bytes_lidos  = xread( &bufferLeituraEscrita[0], 1, 512, &fileDentro );
+        if(!bytes_lidos){
+            break;
+        }else{
+            fwrite(bufferLeituraEscrita, sizeof(unsigned char), bytes_lidos, arquivoSaida);
+        }
+    }
+    fclose(arquivoSaida);
+    xclose(&fileDentro);
+}
+
+
+/*
+    Esta função serve para gravar a imagem de um disco FAT32.
+    Parametros:
+        nomeDoArquivo: Nome do arquivo que sera gravado dentro do disco FAT32.
+        tamanhoSetores: Representa a quantidade de Bytes que cada setor do disco tem. Normalmente 512bytes.
+        quantidadeClusters: Representa a quantidade de cluster no disco FAT32. Cada cluster contem a mesma quantidade de setores.
+        quantidadeSetoresPorCluster: Representa a quantidade de setores contidos dentro de um cluster como 1, 2, 4, 8, 16, 32, 64 ou 128 setores
+*/
+void criarDisco(char* nomeDoArquivo, int tamanhoSetores, int quantidadeClusters, int quantidadeSetoresPorCluster){
+    
+    /*
+        Tamanho do sistema de arquivos
+        Cada cluster pode ter 1, 2, 4, 8, 16, 32, 64 ou 128 setores
+        Cada setor pode ter 128, 256, 512 ou 1024 bytes
+        A quantidade de entradas FAT é igual a quantidade de clusters * 2 pois existem duas FATs
+    */
+    int quantidadeDataSectors = quantidadeClusters * quantidadeSetoresPorCluster;
+    int quantidadeSetoresReservados = 32;
+
+
+    /* 
+        Abrir o buffer para criar o arquivo de imagem do FAT32 
+    */
+    FILE *file = fopen(nomeDoArquivo,"wb");
+
+
+
+    /* 
+       Colocando os 32 setores reservados no inicio do FAT32
+       Estes setores representam o Boot Sector, FS Information Sector e Additional Reserved Sectors (Optional)
+    */
+
+    // Boot Sector / Bios Parameter Block
+    // OBS: Consultar a página 3 do arquivo docs/FAT Filesystem.pdf para melhores explicações.
+    BlocoParametrosBios BPB;
+    memset(&BPB, 0, sizeof(struct blocoParametrosBios));
+    BPB.byterPorSetor = tamanhoSetores;          
+    BPB.setoresPorCluster = quantidadeSetoresPorCluster;        
+    BPB.setoresReservados = 32;   
+    BPB.numeroFats = 2;           
+    BPB.entradasRoot = 0;         
+    BPB.quantidadeSetores16 = 0;    
+    BPB.descricaoMidia = 0xf0;
+    BPB.setoresPorFat = 0; 
+    BPB.setoresPorTrack = 0;        
+    BPB.numeroDeCabecas = 0;         
+    BPB.setoresEscondidos = 0;         
+    BPB.quantidadeSetores32 = ( quantidadeClusters * quantidadeSetoresPorCluster ) + ( 512 * quantidadeSetoresReservados ) + (( quantidadeClusters * 32 ) * 2);
+    BPB.setoresFat = ( ( quantidadeClusters * 32 ) * 2 );     
+    BPB.bandeirasEspelhamento = 0;
+    BPB.versao = 0;            
+    BPB.clusterRoot = 1;             
+    BPB.setorDoFSInformation = 2;    
+    BPB.setorDoBackup = 0;           
+    BPB.numeroDriveFisico = 0;       
+    BPB.assinaturaExBoot = 0;        
+    BPB.numeroSerialVolume = 0;     
+    strcpy(BPB.labelDoVolume,"_testeFat32");       
+    strcpy(BPB.tipoSistemaDeArquivos,"FAT32   ");
+    BPB.assinaturaBoot = 0xaa55;     
+    fwrite(&BPB,512,1,file);
+
+    // File System Information Sector
+    SetorInformacoesFS informacoesFS;
+    memset(&informacoesFS, 0, sizeof(struct setorInformacoesFS));
+    informacoesFS.leadSignature = 0x41615252;         
+    informacoesFS.structSignature = 0x61417272;            
+    informacoesFS.contadorClusterLivre = 0;
+    informacoesFS.contadorProximoClusterLivre = 0;
+    informacoesFS.assinaturaDaTrilha = 0xaaff0000; 
+    fwrite(&informacoesFS,sizeof(struct setorInformacoesFS),1,file);
+
+
+    // setores reservados opcionais
+    for( int i = 0 ; i < quantidadeSetoresReservados - 2; i++ ){
+        unsigned char *setorReservado = genSetorVazio(tamanhoSetores);
+        fwrite(setorReservado,sizeof(unsigned char),tamanhoSetores,file);
+        free(setorReservado);
+    }
+
+
+    /*
+        Colocando as entradas FAT32.
+        As entradas são compostas de structs de 32bytes.
+        O numero de entradas é igual ao dobro do numero de clusters pois existem duas tabelas FAT32.
+        OBS: O primeiro cluster é ocupado pelo diretorio root nas duas tabelas
+    */
+    EntradaFAT entradaRoot;
+    memset(&entradaRoot, 0, sizeof(EntradaFAT));
+    entradaRoot.ponteiro = 0xffffffff;
+    for( int j = 0 ; j < 2 ; j++ ){
+        for( int i = 0 ; i < ( 512 / 4 ); i++ ){
+            if( i == 0 ){
+                fwrite(&entradaRoot,sizeof(EntradaFAT),1,file);
+            }else{
+                EntradaFAT entrada;
+                memset(&entrada, 0, sizeof(EntradaFAT));
+                fwrite(&entrada,sizeof(EntradaFAT),1,file);
+            }
+            
+        }
+    }    
+    
+    /*
+        Criando tabela Root
+    */
+    for( int i = 1 ; i < 16 ; i++ ){
+        EntradaDiretorio entrada;
+        memset(&entrada, 0, sizeof(EntradaDiretorio));
+        fwrite(&entrada,sizeof(EntradaDiretorio),1,file);
+    }
+
+
+    /*
+        Adicionando os Clusters ao disco.
+        O numero de setores adicionados será igual ao numero de clusters * o numero de setores por cluster.
+    */
+    for( int i = 0 ; i < quantidadeClusters - 1 ; i++ ){
+        for( int j = 0 ; j < quantidadeSetoresPorCluster ; j++ ){
+            unsigned char *setorDoCluster = genSetorVazio(tamanhoSetores);
+            fwrite(setorDoCluster,sizeof(unsigned char),tamanhoSetores,file);
+            free(setorDoCluster);
+        }
+    }
+
+    // fechar o arquivo de imagem
+    fclose(file);
+
+}
---
+++ includes/xutils.c
@@ -0,0 +1,372 @@
+/*
+    formatar o nome de um arquivo para o formato do FAT32
+*/
+
+char * aplicarFormatacaoNome(char * nomeArquivo) {
+    char nome[9]; // Espaço para 8 caracteres + caractere nulo
+    char extensao[4]; // Tamanho máximo da extensão do arquivo (defina como necessário)
+
+    int tamanho = strlen(nomeArquivo);
+    int i = 0;
+    int j = 0;
+
+    // Copiar o nome do arquivo para 'nome'
+    while (nomeArquivo[i] != '.' && i < 8) {
+        nome[i] = nomeArquivo[i];
+        i++;
+    }
+    if( i < 8 ){
+        int caracteresFaltando = 8 - i;
+        int posicaoNome = i;
+        for( int caractereFaltando = 0 ; caractereFaltando < caracteresFaltando ; caractereFaltando++ ){
+            nome[posicaoNome] = ' ';
+            posicaoNome++;
+        }
+    }
+    i++;
+
+    for( ; j < 3 ; j++,i++ ){
+        extensao[j] = nomeArquivo[i];
+    }
+    j++;
+
+    // Extrair a extensão do arquivo
+    char nomeFormatado[12]; 
+    for( int caractere = 0; caractere < 8 ; caractere++ ){
+        nomeFormatado[caractere] = nome[caractere];
+    }
+    for( int caractere = 0, posicaoFinal = 8; caractere < 3 ; caractere++, posicaoFinal++  ){
+        nomeFormatado[posicaoFinal] = extensao[caractere];
+        if(caractere == 2){
+            posicaoFinal++;
+            nomeFormatado[posicaoFinal] = '\0';
+        }
+    }
+    
+    char * retorno = (char *)(nomeFormatado);
+
+    return retorno;
+}
+
+/* 
+    remover a formatacao de nome arquivo de uma entradaDeDiretorio
+*/
+char * removerFormatacaoNome( char * nomeFormatado ){
+
+    // array para conter todos os caracteres do nome apos a retirada da formatacao
+    char nomeSemFormatacao[20];
+
+    // pegar o nome do arquivo sem os espacoes e a extensao
+    int i = 0;
+    int j = 0;
+    for( ; i < 8 ; i++ ){
+        if( nomeFormatado[i] != ' '){
+            nomeSemFormatacao[j] = nomeFormatado[i];
+            j++;
+        }
+    }
+    nomeSemFormatacao[j] = '.';
+    j++;
+
+    //pegar a extensao do arquivo
+    for( ; i < 11 ; i++ ){
+        if( nomeFormatado[i] != ' '){
+            nomeSemFormatacao[j] = nomeFormatado[i];
+            j++;
+        }
+    }
+    nomeSemFormatacao[j] = '\0';
+    
+    //converter array de chars em uma string
+    char * retorno = (char *)(nomeSemFormatacao);    
+    return retorno;
+
+}
+
+
+/*
+    Verificar se o arquivo já existe ou deve ser criado
+*/
+int arquivoExiste( FILE* disco, char * nomeArquivo){
+    // acessar a tabela do diretorio root
+    int inicioEntradas = ( 32 * 512 ) + 1024;
+    EntradaDiretorio entradaLimpa;
+    memset(&entradaLimpa, 0, sizeof(EntradaDiretorio));
+
+    /* 
+      criar struct de entrada apartir das entradas da root
+      Para isso é necessário procurar o arquivo pelo nome entre as entrada da root 
+    */
+    int proximoCluster;
+    int quantidadeEntradas = 0;
+
+    while( quantidadeEntradas <= 16 ){
+
+        fseek(disco, inicioEntradas, SEEK_SET);
+        EntradaDiretorio entradaTeste;
+        fread(&entradaTeste, sizeof(EntradaDiretorio), 1, disco);
+
+        // Diminuir o tamanho do nome do arquivo para 11
+        char nome[11];
+        strncpy(nome, entradaTeste.filename, 11);
+        int comparacao = strcmp(nome,nomeArquivo);
+
+        // comparando o nome do arquivo com o nome da entrada para ver se são iguais
+        // OBS: strcmp retorna zero quando as strings são iguais. Diferente doque poderiamos pensar.
+        if(comparacao == 0){
+            return 1;
+        }
+        else{
+            inicioEntradas += 32;
+        }
+        quantidadeEntradas++;
+    }
+    return 0;
+}
+
+/*
+    Esta função serve para retornar o numero de um cluster livre para podermos iniciar a gravação do arquivo
+    OBS: É importante se notar que, os cluster validos começam a partir do terceiro cluster. Os dois primeiros não podem ser usados.
+    Parametros:
+        file: É o buffer da imagem do disco FAT32 onde os clusters estão.
+*/
+int acharClusterVazio( FILE* disco, int descartar){
+
+    int posicao = 1;
+    int inicioFat = ( 512 * 32 ) + 4;
+
+    while(1){
+
+        if( posicao == descartar ){
+            posicao++;
+            continue;
+        }
+
+        fseek(disco,inicioFat, SEEK_SET);
+        EntradaFAT entrada;
+        fread(&entrada, sizeof(EntradaFAT), 1, disco);
+
+        if(!entrada.ponteiro){
+            break;
+        }
+        else{
+            inicioFat += 4;
+            posicao++;
+        }
+    }
+    return posicao;
+}
+
+
+/*
+    Esta função serve para procurar uma entrada disponivel no diretorio root.
+    No futuro ela recebe tambem um nome de diretório para que possamos procurar entradas dentro deste diretório.
+    Parametros:
+        disco: buffer de uma imagem de disco FAT32
+*/
+int acharEntradaVazia( FILE* disco ){
+    int inicioRoot = ( 512 * 32 ) + 1024;
+    int posicao = 0;
+
+    while(1){
+
+        fseek(disco,inicioRoot, SEEK_SET);
+        EntradaDiretorio entrada;
+        fread(&entrada, sizeof(EntradaDiretorio), 1, disco);
+
+        if(entrada.atributos == 0){
+            break;
+        }
+        else{
+            inicioRoot += 32;
+            posicao++;
+        }
+    }
+    return posicao;
+}
+
+
+/*
+    Pegar a posicao de uma entrada de diretorio
+*/
+int pegarPosicaoEntradaDeDiretorio( FILE* disco, char * nomeArquivo){
+    // acessar a tabela do diretorio root
+    int inicioEntradas = ( 32 * 512 ) + 1024;
+    EntradaDiretorio entradaLimpa;
+    memset(&entradaLimpa, 0, sizeof(EntradaDiretorio));
+
+    /* 
+      criar struct de entrada apartir das entradas da root
+      Para isso é necessário procurar o arquivo pelo nome entre as entrada da root 
+    */
+    int proximoCluster;
+    int quantidadeEntradas = 0;
+    int posicao = 0;
+
+    while( quantidadeEntradas <= 16 ){
+
+        fseek(disco, inicioEntradas, SEEK_SET);
+        EntradaDiretorio entradaTeste;
+        fread(&entradaTeste, sizeof(EntradaDiretorio), 1, disco);
+
+        // Diminuir o tamanho do nome do arquivo para 11
+        char nome[11];
+        strncpy(nome, entradaTeste.filename, 11);
+        int comparacao = strcmp(nome,nomeArquivo);
+
+        // comparando o nome do arquivo com o nome da entrada para ver se são iguais
+        // OBS: strcmp retorna zero quando as strings são iguais. Diferente doque poderiamos pensar.
+        if(comparacao == 0){
+            return posicao;
+        }
+        else{
+            inicioEntradas += 32;
+        }
+        quantidadeEntradas++;
+        posicao++;
+    }
+}
+
+
+/*
+    Pegar entrada de um arquivo
+*/
+EntradaDiretorio pegarEntradaDeDiretorio( FILE* disco, char * nomeArquivo){
+    // acessar a tabela do diretorio root
+    int inicioEntradas = ( 32 * 512 ) + 1024;
+    EntradaDiretorio entradaLimpa;
+    memset(&entradaLimpa, 0, sizeof(EntradaDiretorio));
+
+    /* 
+      criar struct de entrada apartir das entradas da root
+      Para isso é necessário procurar o arquivo pelo nome entre as entrada da root 
+    */
+    int proximoCluster;
+    int quantidadeEntradas = 0;
+
+    while( quantidadeEntradas <= 16 ){
+
+        fseek(disco, inicioEntradas, SEEK_SET);
+        EntradaDiretorio entradaTeste;
+        fread(&entradaTeste, sizeof(EntradaDiretorio), 1, disco);
+
+        // Diminuir o tamanho do nome do arquivo para 11
+        char nome[11];
+        strncpy(nome, entradaTeste.filename, 11);
+        int comparacao = strcmp(nome,nomeArquivo);
+
+        // comparando o nome do arquivo com o nome da entrada para ver se são iguais
+        // OBS: strcmp retorna zero quando as strings são iguais. Diferente doque poderiamos pensar.
+        if(comparacao == 0){
+            return entradaTeste;
+        }
+        else{
+            inicioEntradas += 32;
+        }
+        quantidadeEntradas++;
+    }
+}
+
+
+/*
+    A Funcao a baixo retorna uma lista com 512bytes zerados.
+    Isso eh usado sempre quano é necessário escrever um setor no disco.
+    Em algumas ocasioes a lista retornada é alterada antes de ser gravada no disco.
+    Parametros:
+        tamanhoSetores: indica a quantidade de bytes contidos dentro do setor.
+*/
+unsigned char* genSetorVazio( int tamanhoSetores ) {
+    unsigned char *setor = (unsigned char *)malloc( tamanhoSetores * sizeof(unsigned char) );
+
+    for (int i = 0; i < tamanhoSetores ; i++) {
+        setor[i] = 0x0;
+    }
+
+    return setor;
+}
+
+
+/*
+    Colocar a tabela FAT dentro de um array para ser mais facil de analizar
+*/
+int* fatToArray(FILE * disco) {
+    int* fat = malloc(128 * sizeof(unsigned int));
+    int inicioFat = 512 * 32;
+    fseek(disco, inicioFat, SEEK_SET);
+    fread(&fat[0], sizeof(unsigned int), 128, disco);
+    //printf("\nSTART FAT\n");
+    //for( int i = 0 ; i < 128 ; i++ ){
+    //    printf(" %d ",fat[i]);
+    //}
+    //printf("\nEND FAT\n");
+    return fat;
+}
+
+
+/*
+    Colocar a tabela FAT dentro de um array para ser mais facil de analizar
+*/
+void printarFat(char * nomeDisco) {
+    FILE * disco = fopen(nomeDisco,"r");
+    int* fat = malloc(128 * sizeof(unsigned int));
+    int inicioFat = 512 * 32;
+    fseek(disco, inicioFat, SEEK_SET);
+    fread(&fat[0], sizeof(unsigned int), 128, disco);
+    printf("\nSTART FAT\n");
+    for( int i = 0 ; i < 128 ; i++ ){
+        printf(" %d ",fat[i]);
+    }
+    printf("\nEND FAT\n");
+    fclose(disco);
+}
+
+
+/*
+printar a struct de de chamada de sistemas open
+*/
+void printarOpen(XFILE file){
+    printf("nome arquivo: %s\n",file.filename);
+    printf("tamanho arquivo: %d\n",file.tamanhoArquivo);
+    printf("quantidade clusters: %d\n",file.qnt_cluster);
+    printf("posicao no arquivo: %d\n",file.posicaoNoArquivo);
+    printf("Clusters do arquivo: ");
+    for( int i = 0 ; i < file.qnt_cluster ; i++ ){
+        printf("%d > ",file.clusters[i]);
+    }
+    printf("\n");
+}
+
+
+/*
+printar a struct de entradaDeDiretorio
+*/
+void printEntradaDiretorio(EntradaDiretorio file){
+
+    printf("nome arquivo: %s\n",file.filename);
+    printf("atributos: %02X\n",file.atributos);
+    printf("tempo: %d\n",file.time);
+    printf("data: %d\n",file.date);
+    printf("primeiro cluster: %d\n",file.startCluster);
+    printf("tamanho: %d\n",file.fileSize);
+    printf("\n");
+}
+
+
+/*
+    Pegar um cluster e colocar em um array
+*/
+/*
+    A Funcao a baixo retorna uma lista com 512bytes zerados.
+    Isso eh usado sempre quano é necessário escrever um setor no disco.
+    Em algumas ocasioes a lista retornada é alterada antes de ser gravada no disco.
+    Parametros:
+        tamanhoSetores: indica a quantidade de bytes contidos dentro do setor.
+*/
+unsigned char* clusterToArray( int numeroCluster, FILE * disco, int tamanhoCluster ) {
+    unsigned char *cluster = (unsigned char *)malloc( tamanhoCluster * sizeof(unsigned char) );
+    int posicaoCluster = ( 512 * 32 ) + 1024 + ( tamanhoCluster * numeroCluster);
+    fseek(disco, posicaoCluster, SEEK_SET);
+    fread(&cluster[0], sizeof(unsigned char), tamanhoCluster, disco);
+    return cluster;
+}
+
